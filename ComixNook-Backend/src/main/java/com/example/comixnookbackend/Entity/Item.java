ddaa8eb5a221402558ebb58ffae6063536173d7d
Nirajan Mahato <123573405+NirajanMahato@users.noreply.github.com>
2024-02-12 19:19:54 +0545

    added er file

--- ComixNook-Backend/src/main/java/com/example/comixnookbackend/Entity/Item.java
+++ ComixNook-Backend/src/main/java/com/example/comixnookbackend/Entity/Item.java
@@ -1,2 +1,33 @@
-package com.example.comixnookbackend.Entity;public class Item {
+package com.example.comixnookbackend.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name="item")
+public class Item {
+    @Id
+    @SequenceGenerator(name = "users_seq_gen", sequenceName = "users_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "users_seq_gen", strategy = GenerationType.SEQUENCE)
+    private long itemId;
+
+    @Column(name = "item_name", nullable = false, unique = true)
+    private String itemName;
+
+    @Column(name = "release_date")
+    private LocalDate releasedDate;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "item_image")
+    private String itemImage;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genreId;
 }
--- ComixNook-Backend/src/main/java/com/example/comixnookbackend/Pojo/ItemPojo.java
+++ ComixNook-Backend/src/main/java/com/example/comixnookbackend/Pojo/ItemPojo.java
@@ -1,2 +1,31 @@
-package com.example.comixnookbackend.Pojo;public class ItemPojo {
+package com.example.comixnookbackend.Pojo;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPojo {
+
+    private Long id;
+
+    @NotNull
+    private String itemName;
+
+    private LocalDate releasedDate;
+
+    private String author;
+
+    private MultipartFile itemImage;
+
+    private Long genreId;  // Assuming genreIds represent the IDs of associated genres
 }
