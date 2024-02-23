3079fcab641308849af5dec4dedb78e34949ce9d
Nirajan Mahato <123573405+NirajanMahato@users.noreply.github.com>
2024-02-23 21:18:48 +0545

    item test is left

--- ComixNook-Backend/src/test/java/com/example/comixnookbackend/ItemRepositoryTest.java
+++ ComixNook-Backend/src/test/java/com/example/comixnookbackend/ItemRepositoryTest.java
@@ -1,13 +1,15 @@
 package com.example.comixnookbackend;
 
-
 import com.example.comixnookbackend.Entity.Item;
+import com.example.comixnookbackend.Repo.GenreRepo;
 import com.example.comixnookbackend.Repo.ItemRepo;
+import com.example.comixnookbackend.util.ImageToBase64;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
@@ -19,6 +21,11 @@ import java.time.LocalDate;
 public class ItemRepositoryTest {
     @Autowired
     private ItemRepo itemRepo;
+    @Mock
+    private GenreRepo genreRepo;
+
+    @Mock
+    private ImageToBase64 imageToBase64;
 
     @Test
     @Order(1)
@@ -26,11 +33,12 @@ public class ItemRepositoryTest {
     public void saveItem(){
         Item item = new Item();
 
-        item.setItemName("Item image1");
-        item.setAuthor("Robert");
+        item.setItemName("Item Name1");
         item.setReleasedDate(LocalDate.of(2003, 12, 12));
-//        item.setGenreId();
-//        item.setItemImage("image1.jpg");
+        item.setItemDescription("This is the discription of the comic");
+        item.setDownloadLink("https://example.com/download");
+        item.setItemImage(null);
+//        item.setGenreId(1L);
 
         item = itemRepo.save(item);
 
